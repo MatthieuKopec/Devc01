dccc517f14a41f17d3bd758f3998b46d2e8b8392
Cassandra <c.sangkhavongs@gmail.com>
2017-07-26 13:54:26 +0200

    coupe coupe mon code

--- Exercice4/my_aff_comb2.c
+++ Exercice4/my_aff_comb2.c
@@ -1,70 +1,85 @@
 
 void my_putchar(char c);
 
-void print_line(char a, char b, char c, char d){
-
+void print_line(char a, char b, char c, char d)
+{
+  if (a==57 && b==56)
+    {
   my_putchar(a);
   my_putchar(b);
   my_putchar(' ');
   my_putchar(c);
   my_putchar(d);
-}
-void end(){
-  my_putchar(',');
+  }
+  
+  else
+    {
+  my_putchar(a);
+  my_putchar(b);
   my_putchar(' ');
+  my_putchar(c);
+  my_putchar(d);
+  my_putchar(',');
+  my_putchar(' ');   
+ }
 }
 
 void first(char a, char b, char c, char d){
-   while(a!=57 || b!=56){
-      if(d==57){
-	if(c==57 && d==57 && b==57){
-	  if(b==56){
+  while (a!=57 || b!=56)   {
+      if (d==57)
+	{
+	if (c==57 && d==57 && b==57)
+	  {
+	  if (b==56)
+	    {
 	    d=49;
 	    b=48;
 	    a++;
 	    c=a;
 	  }
-	  else{
+	  else
+	    {
 	  a++;
 	  b=48;
 	  c=a;
-	  d = b + 1;}	    
+	  d = b + 1;  
 	}
-	else if(c==57 && d==57){
-	  if(b==56){
+	}
+	else if (c==57 && d==57)
+	  {
+          if (b==56)
+	    {
 	    d=48;
 	    b=57;
 	    c = a + 1;
 	  }
-	  else{
+	  else
+	    {
 	  b++;
 	  c=a;
-	  d = b + 1;}
+	  d = b + 1;
+	    }
 	}
-	else if(c==57 && b==57){
+	else if (c==57 && b==57)
+	  {
 	  c=a;
 	  b=48;
 	}
-	else{
+	else
+	  {
 	  c++;
 	  d=48;
 	}
- }
-	else{
-	  d++;
 	}
-      if(a==57 && b==56 && c ==57 && d==57){
-	print_line(a, b, c, d);
-	my_putchar('\n');
-      }
-      else{
-       print_line(a, b, c, d);
-       end();
-      }
-	 }
+      else
+	  d++;
+  print_line(a, b, c, d);
+  }
 }
 
-void my_aff_comb2(){
+
+void my_aff_comb2()
+{
   char a;
   char b;
   char c;
@@ -75,24 +90,9 @@ void my_aff_comb2(){
     c=48;
     d=49;
     
-    while(c!=57 || d!=57){
-      if(d>56){
-	d=48;
-	c++;
-      }
-      else{
-	d++;
-      }
-      print_line(a, b, c, d);
-    }
-
-    b++;
-    print_line(a, b, c, d);
-    c=48;
-    d = b + 1;
     print_line(a, b, c, d);
     first(a, b, c, d);
-
+    my_putchar('\n');
 }
 
 
